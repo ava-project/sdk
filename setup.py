a253e791772733ae55b4f9c5504f65efd9d83ecb
Dorian Amouroux <dor.amouroux@gmail.com>
2017-03-17 00:32:25 -0700

    push setup for pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+keywords = [
+    'ava', 'sdk',
+]
+
+setup(
+    name='ava-sdk',
+    version=sdk.__version__,
+    description=sdk.__doc__,
+    author=sdk.__author__,
+    license='Apache',
+    url=dbbackup.__url__,
+    keywords=keywords,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
