796bf8456926c9baeae8eaf109308b67340c94c4
Tommy <thomas.milox@epitech.eu>
2017-04-23 07:09:00 +0200

    improved error handling

--- avasdk/ioutils/utils.py
+++ avasdk/ioutils/utils.py
@@ -2,6 +2,10 @@ from .exceptions import RuntimeError
 import os, shutil, json, zipfile
 
 # Format the output.
+# @param:
+#   - args: the strings to format
+#
+# @result: format the given string as following '[string1:string2:....:stringN]:'
 def format_output(*args):
     result = "["
     for arg in args:
@@ -10,12 +14,15 @@ def format_output(*args):
     return result[:-1] + str("]: ")
 
 # Removes the given file if it exists, raise an error otherwise.
-# @param: string (/path/to/the /file/to/remove)
+# @param:
+#   - path: string (/path/to/the/file/to/remove)
+#
+# @behave: raise an error if the specified file does not exist.
 def remove_file(path):
     if os.path.isfile(path) == True:
         os.remove(path)
     else:
-        raise RuntimeError('invalid path')
+        raise RuntimeError(__name__, remove_file.__name__, "Cannot remove (" + path + "), no such file.")
 
 
 # Handler for removing a directory and all its content
@@ -27,7 +34,7 @@ def remove_directory(path):
     if os.path.isdir(path) == True:
         shutil.rmtree(path)
     else:
-        raise RuntimeError('Invalid path')
+        raise RuntimeError(__name__, remove_directory.__name__, "Cannot remove (" + path + "), no such directory.")
 
 
 # Unzip the file pointed by 'path' to extract its to content to the given destination
@@ -36,13 +43,24 @@ def remove_directory(path):
 #   - destination: string (/path/to/extract/the/zip)
 #
 # @behave: raise an error if either the path or the destination is invalid.
+# @behave: raise an error if the archive is corrupted.
 def unzip(path, destination):
-    if os.path.isfile(path) == True and os.path.isdir(destination) == True:
-        zip_ref = zipfile.ZipFile(path, 'r')
-        zip_ref.extractall(destination)
-        zip_ref.close()
+    if os.path.isfile(path) == True:
+
+        if os.path.isdir(destination) == True
+            archive = zipfile.ZipFile(path, 'r')
+            result = archive.testzip()
+
+            if result is not None:
+                raise RuntimeError(__name__, unzip.__name__, "Error corrupted archive.")
+
+            archive.extractall(destination)
+            archive.close()
+
+        else:
+            raise RuntimeError(__name__, unzip.__name__, "Invalid path (" + destination + "), no such directory.")
     else:
-        raise RuntimeError('Failed to unzip at the given location')
+        raise RuntimeError(__name__, unzip.__name__, "Invalid path (" + path + "), no such file.")
 
 
 # Parse a json file and store key and value into the given dictionary.
@@ -50,7 +68,7 @@ def unzip(path, destination):
 #   - path: string (/path/to/the/directory/containing/the/json/file)
 #   - dictionary: the dictionary to fill with data.
 #
-# @behave: display an error message in case of invalid path to directory containing a json file.
+# @behave: raise an error if the path is not pointing to a directory.
 def parse_json_file_to_dictionary(path, dictionary):
     if os.path.isdir(path) == True:
         for file in os.listdir(path):
@@ -60,4 +78,4 @@ def parse_json_file_to_dictionary(path, dictionary):
                 for key, value in data.items():
                     dictionary[key] = value
     else:
-        print(format_output(__name__, parse_json_file_to_dictionary.__name__) + "No such file or directory: " + path)
+        raise RuntimeError(__name__, parse_json_file_to_dictionary.__name__, "Invalid path (" + path + "), no such directory.")
